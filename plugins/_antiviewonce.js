e368dd836d3cf81f29956486dc90bdda17442690
Lizbeth <107339444+Lizbot123@users.noreply.github.com>
2025-03-06 11:22:13 -0600

    Update _antiviewonce.js

--- plugins/_antiviewonce.js
+++ plugins/_antiviewonce.js
@@ -1,35 +1,16 @@
-let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'))
+let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'));
 
-let handler = m => m
-handler.before = async function (m, { conn, isAdmin, isBotAdmin }) {
-let media, msg, type
-const { antiver, isBanned } = global.db.data.chats[m.chat]
-if (!antiver || isBanned || !(m.mtype == 'viewOnceMessageV2' || m.mtype == 'viewOnceMessageV2Extension')) return
-if (m.mtype == 'viewOnceMessageV2' || m.mtype == 'viewOnceMessageV2Extension') {
-msg = m.mtype == 'viewOnceMessageV2' ? m.message.viewOnceMessageV2.message : m.message.viewOnceMessageV2Extension.message 
-type = Object.keys(msg)[0]
-if (m.mtype == 'viewOnceMessageV2') {
-media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'videoMessage' ? 'video' : 'audio')
-} else {
-media = await downloadContentFromMessage(msg[type], 'audio')
-}
-let buffer = Buffer.from([])
-for await (const chunk of media) {
-buffer = Buffer.concat([buffer, chunk])}
-const fileSize = formatFileSize(msg[type].fileLength)
-const description = `
-✅️ *ANTI VER UNA VEZ* ✅️\n\n💭 *No ocultes* ${type === 'imageMessage' ? '`Imagen` 📷' : type === 'videoMessage' ? '`Vídeo` 🎥' : type === 'audioMessage' ? '`Mensaje de voz` 🎤' : 'este mensaje'}\n- 👤 *Usuario:* *@${m.sender.split('@')[0]}*
-${msg[type].caption ? `- *Texto:* ${msg[type].caption}` : ''}`.trim()
-if (/image|video/.test(type)) return await conn.sendFile(m.chat, buffer, type == 'imageMessage' ? 'error.jpg' : 'error.mp4', description, m, false, { mentions: [m.sender] })
-if (/audio/.test(type)) { 
-await conn.reply(m.chat, description, m, { mentions: [m.sender] }) 
-await conn.sendMessage(m.chat, { audio: buffer, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true }, { quoted: m })
-}
+let handler = async (m, { conn }) => {
+if (!m.quoted) return conn.reply(m.chat, `🍬 Responde a una imagen ViewOnce.`, m)
+if (!m?.quoted || !m?.quoted?.viewOnce) return conn.reply(m.chat, `🍬 Responde a una imagen ViewOnce.`, m)
+let buffer = await m.quoted.download(false);
+if (/videoMessage/.test(m.quoted.mtype)) {
+return conn.sendFile(m.chat, buffer, 'media.mp4', m.quoted.caption || '', m)
+} else if (/imageMessage/.test(m.quoted.mtype)) {
+return conn.sendFile(m.chat, buffer, 'media.jpg', m.quoted?.caption || '', m)
 }}
-export default handler
+handler.help = ['ver']
+handler.tags = ['tools']
+handler.command = ['readviewonce', 'read', 'readvo'] 
 
-function formatFileSize(bytes) {
-const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'TY', 'EY']
-const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
-return Math.round(100 * (bytes / Math.pow(1024, i))) / 100 + ' ' + sizes[i]
-}
+export default handler
